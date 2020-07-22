587c1ab75bd43e428477f0b16e307645a7d14b4f
shikhindahikar <shikhindah@gmail.com>
2020-07-22 22:02:38 +0530

    Added mod previleges and sticky alongwith other bug fixes and changes
    Used comment.author.name first time, use this to improve code more

--- Bot.py
+++ Bot.py
@@ -9,12 +9,15 @@ import praw
 import datetime
 from statistics import mean
 import re
+from pytz import timezone
 
+tz = timezone('EST')
 grade_types = {'PO01':1, 'FR02':2, 'AG03':3, 'G04':4, 'G06':6, 'VG08':8, 'VG10':10, 'F12':12, 'F15':15, 'VF20':20, 'VF25':25, 'VF35':35, 'VF30':30, 'XF40':40, 'XF45':45, 'AU50':50, 'AU53':53, 'AU55':55, 'AU58':58, 'MS60':60, 'MS61':61, 'MS62':62, 'MS63':63, 'MS64':64, 'MS65':65, 'MS66':66, 'MS67':67, 'MS68':68, 'MS69':69, 'MS70':70, 'PO':1, 'po':1, 'Poor':1, 'poor':1, 'FR':2, 'fr':2, 'AG':3, 'ag':3, 'G':4, 'g':4, 'Good':4, 'good':4, 'VG':8, 'vg':8, 'F':12, 'f':12, 'Fine':12, 'fine':12, 'VF':20, 'vf':20, 'XF':40, 'xf':40, 'AU':50, 'au':50, 'BU':60, 'bu':60, 'MS':60, 'ms':60}
 
 alt_grade_types = ['PO', 'po', 'Poor', 'poor', 'FR', 'fr', 'AG', 'ag', 'G', 'g', 'Good', 'good', 'VG', 'vg', 'F', 'f', 'Fine', 'fine', 'VF', 'vf', 'XF', 'xf', 'au', 'AU','BU', 'bu', 'MS', 'ms']
 
 numeric_grade_type = [1, 2, 3, 4, 6, 8, 10, 12, 15, 20, 25, 35, 30, 40, 45, 50, 53, 55, 58, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70 ]
+
 reddit = praw.Reddit(user_agent="CoinGrade bot 0.1",
                      client_id="ik7ILQHFETC7QQ", client_secret="8fMRTIFaxMVOnXsUng94OYMkQdA",
                      username="coin_grader_bot", password="yitzchokrcgbot")
@@ -70,7 +73,8 @@ while(True):
                         break
                 
                 if(not in_list):
-                    mycomment = submission.reply("Hi, I’m the RCG bot!\n\nThis comment will be updated every five minutes to include the rounded average of the grades submitted by the community.\n\nTo have your grade counted, please put your grade and designation in square brackets, like this: [MS62 rd] [xf40 cleaned] [f12+] etc, and continue writing any other descriptions and opinions outside the brackets.\n\nThank you, and happy collecting!\n\nLast updated: ")
+                    mycomment = submission.reply("Hi, I’m the RCG bot!\n\nThis comment will be updated every five minutes to include the rounded average of the grades submitted by the community.\n\nTo have your grade counted, please put your grade and designation in square brackets, like this: [MS62 rd] [xf40 cleaned] [f12+] etc, and continue writing any other descriptions and opinions outside the brackets.\n\nThank you, and happy collecting!\n\n ")
+                    mycomment.mod.distinguish(how='yes', sticky=True)               #distinguishes as a mod and sticks the comment
                     comment_id = mycomment.id
                     track_comments.append(comment_id)                       #tracking comment to edit
                     obj = sub_track(submission.id, comment_id)              #creating objects for  tracking
@@ -81,7 +85,7 @@ while(True):
                 numeric_grade = []
                 
                 for comment in submission.comments:
-                    if (comment.score >= 0 and  comment.id and (comment.id not in track_comments)):
+                    if (comment.score >= 0 and  comment.id and (comment.id not in track_comments) and (comment.author.name.lower()!="coin_grader_bot")):            #use author name, improve bot using this functionality 
                         curr_comment = comment.body
                         sub_str = re.findall(r'\[(.*?)\]', curr_comment)
                         if(sub_str):
@@ -92,7 +96,7 @@ while(True):
                             else:
                                 for alt in alt_grade_types:
                                     if(alt in sub_str[0]):                      #po and poor bug
-                                        if((alt=='po' and 'poor' in sub_str[0]) or (alt=='f' and 'fine' in sub_str[0]) or (alt=='F' and 'Fine' in sub_str[0])):
+                                        if((alt=='po' and 'poor' in sub_str[0]) or (alt=='f' and 'fine' in sub_str[0]) or (alt=='F' and 'Fine' in sub_str[0]) or (alt=='F' and 'VF' in sub_str[0]) or (alt=='f' and 'vf' in sub_str[0]) or (alt=='G' and 'VG' in sub_str[0]) or (alt=='g' and 'vg' in sub_str[0]) or (alt=='F' and 'XF' in sub_str[0]) or (alt=='f' and 'xf' in sub_str[0])):
                                             continue    
                                         else: 
                                             numeric_grade.append(grade_types[alt])
@@ -112,7 +116,7 @@ while(True):
                     average = int(mean(numeric_grade))
                     print(average)
                     final_avg = search_avg_grade(average)                 #finds the correspoding key in grade_types
-                    edited_comment = mycomment.body + "\nThis coin, according to the community, grades as follows: " + final_avg + "\n\n*Last updated*:" + datetime.datetime.now().strftime('%d/%m/%y %I:%M %S %p')
+                    edited_comment = "Hi, I’m the RCG bot!\n\n" + "This coin, according to the community, grades as follows: \n #" + final_avg +"\n\nThis comment will be updated every five minutes to include the rounded average of the grades submitted by the community.\n\nTo have your grade counted, please put your grade and designation in square brackets, like this: [MS62 rd] [xf40 cleaned] [f12+] etc, and continue writing any other descriptions and opinions outside the brackets.\n\nThank you, and happy collecting!\n\n " + "\n\n*Last updated*: " + datetime.datetime.now(tz).strftime('%d/%m/%y %I:%M:%S %p')
                 
                     for x in submission_track:
                         if (x.submission_id == submission.id):
